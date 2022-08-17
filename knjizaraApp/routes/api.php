0bb05ec412920e6098cad30b15b17a71451890a3
nevenafundup <nevenafundup7@gmail.com>
2022-08-17 23:52:33 +0200

    kreirana osnovna api ruta

--- knjizaraApp/routes/api.php
+++ knjizaraApp/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProizvodController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +14,16 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+//get
+//put
+//post
+//delete
+Route::get('/proizvodi',[ProizvodController::class,'index']);
+Route::get('/proizvodi/{id}',[ProizvodController::class,'show']);
+
+
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
