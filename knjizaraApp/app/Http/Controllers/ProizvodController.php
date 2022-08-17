a75da172e90c4db584a93c62575e3bca921a5e2c
nevenafundup <nevenafundup7@gmail.com>
2022-08-17 23:44:37 +0200

    kreirani osnovni kontroleri

---
+++ knjizaraApp/app/Http/Controllers/ProizvodController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Proizvod;
+use Illuminate\Http\Request;
+
+class ProizvodController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Proizvod::all();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Proizvod  $proizvod
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return Proizvod::find($id);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Proizvod  $proizvod
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Proizvod $proizvod)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Proizvod  $proizvod
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Proizvod $proizvod)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Proizvod  $proizvod
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Proizvod $proizvod)
+    {
+        //
+    }
+}
